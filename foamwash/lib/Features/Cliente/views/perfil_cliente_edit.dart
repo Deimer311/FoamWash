848d56ac2ab86b43df451837c7cec5603e14eac3
cristian-andres-criollo <tovarcristian431@gmail.com>
2026-07-09 16:33:28 -0500

    carga solucion errores en movil

--- foamwash/lib/Features/Cliente/views/perfil_cliente_edit.dart
+++ foamwash/lib/Features/Cliente/views/perfil_cliente_edit.dart
@@ -7,6 +7,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:foamwash/Api/api_constants.dart';
 import 'package:http/http.dart' as http;
--- foamwash/lib/Features/Trabajador/views/perfil_trabajador_edit.dart
+++ foamwash/lib/Features/Trabajador/views/perfil_trabajador_edit.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:foamwash/Api/api_constants.dart';
 import 'package:http/http.dart' as http;
